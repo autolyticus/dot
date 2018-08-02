dd4aed1e503e6d363496f70b5842f6a463475f33
reisub0 <reisub0@gmail.com>
2018-08-02 13:04:33 +0530

    Fixed apack having no effect

--- .config/fish/functions/apack.fish
+++ .config/fish/functions/apack.fish
@@ -1,8 +1,6 @@
 function apack
-	pacman -S --needed "$argv"; and fish -c '
-		echo "$argv" >>~/.local/.packlist
-		sort ~/.local/.packlist | uniq >~/.local/.packlist.new
-		mv -f ~/.local/.packlist.new ~/.local/.packlist
-    '
+	pacman -S --needed "$argv"; and echo "$argv" >>~/.local/.packlist
+    sort ~/.local/.packlist | uniq >~/.local/.packlist.new
+    mv -f ~/.local/.packlist.new ~/.local/.packlist
 end
 
