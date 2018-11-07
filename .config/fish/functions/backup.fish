2f48bd8f9939232c4595816bd066db33a96e4ce9
reisub0 <reisub0@gmail.com>
2018-11-07 17:20:48 +0530

    Made backup a binary
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/functions/backup.fish
+++
@@ -1,6 +0,0 @@
-function backup -d 'Backup entire root to LBackup partition'
-	sudo mount /media/LBackup; and 
-	sudo rsync -aAXvr --partial /* /media/LBackup --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/tmp,/var/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/var,/home/g/Downloads,/boot,'*.cache*'}
-	sudo umount /media/LBackup
-end
-
