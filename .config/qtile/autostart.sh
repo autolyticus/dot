d966741d1be61de7681b2171ac86af5827810d03
reisub0 <reisub0@gmail.com>
2018-11-15 00:16:41 +0530

    Made redshift a bit redder

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -10,6 +10,6 @@ runCommand feh --bg-scale ~/.config/awesome/themes/multicolor/vio.jpg
 runCommand mpd
 runCommand nm-applet
 runCommand numlockx
-runCommand redshift -l 20.5937:78.9629
+runCommand redshift -l 20.5937:78.9629 -t 6500:3000
 runCommand rescuetime
 runCommand unclutter -root
