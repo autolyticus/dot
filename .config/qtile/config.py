fcc62bdda71424487191b1cef8e51c06d0e43392
reisub0 <reisub0@gmail.com>
2018-11-15 00:16:57 +0530

    More shortcuts ported from awesome, including rofi ones, also minor cleanup

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -29,32 +29,39 @@ keys = [
     # Switch window focus to other pane(s) of stack
     Key([mod], "space", lazy.next_layout()),
     Key([alt], "Tab", lazy.function(user.PrevFocus())),
+    Key([mod], "q", lazy.window.kill()),
+    Key([mod, "control"], "f", lazy.window.toggle_floating()),
+    Key([mod], "f", lazy.window.toggle_fullscreen()),
+
+    # Rofi keys
+    Key([mod], "s", lazy.spawn('''bash -c 'termite --class "fzf-menu" -e ~/.local/bin/songChooser' ''')),
+    Key([mod], "g", lazy.spawn('rofi -show run')),
+    Key([mod], "r", lazy.spawn('rofi -modi combi -show combi -combi-modi drun,run')),
+
+    # Launchers
+    Key([mod], "Return", lazy.spawn(terminal)),
+    Key([mod], "KP_Enter", lazy.spawn(terminal)),
+    Key([mod], "w", lazy.spawn(browser)),
+
+    # Management
+    Key([mod, "control"], "s", lazy.spawn('susp')),
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "l", lazy.spawn('locker')),
+    Key([mod, "control"], "q", lazy.shutdown()),
+
     # Key([mod, "shift"], "space", lazy.next_()),
     # Toggle between different layouts as defined below
 
     # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate()),
+    # Key([mod, "shift"], "space", lazy.layout.rotate()),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
-    Key([mod], "Return", lazy.spawn(terminal)),
-    Key([mod], "KP_Enter", lazy.spawn(terminal)),
-    Key([mod], "w", lazy.spawn(browser)),
+    # Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
 
-    Key([mod], "q", lazy.window.kill()),
-    Key([mod, "control"], "f", lazy.window.toggle_floating()),
-    Key([mod], "f", lazy.window.toggle_fullscreen()),
 
-    Key([mod, "control"], "l", lazy.shutdown()),
-    Key([mod, "control"], "s", lazy.spawn('susp')),
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.spawncmd()),
-
-    # Key([], 'XF86AudioRaiseVolume', lazy.spawn('termite')),
 ]
 
 groups = [Group(i) for i in "123456"]
