5289f171bf3c7834cca1aa7ede4901678c666ffd
reisub0 <reisub0@gmail.com>
2018-11-15 00:17:57 +0530

    Added fzf-menu to wmclass that is floating
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -152,6 +152,7 @@ floating_layout = layout.Floating(float_rules=[
     {'wname': 'branchdialog'},  # gitk
     {'wname': 'pinentry'},  # GPG key password entry
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
+    {'wmclass': 'fzf-menu'},  # ssh-askpass
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
