eb99131bbcbb1fb0a7cd0fb792f41d1e87bdc352
reisub0 <reisub0@gmail.com>
2018-11-30 02:03:27 +0530

    Added songChooser
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -35,7 +35,8 @@ keys = [
 
     # Rofi keys
     Key([mod], "d", lazy.spawn('rofi -show window')),
-    Key([mod], "s", lazy.spawn('''bash -c 'termite --class "fzf-menu" -e ~/.local/bin/songChooser' ''')),
+    Key([mod], "s", lazy.spawn(
+        '''bash -c 'termite --class "fzf-menu" -e ~/.local/bin/songChooser' ''')),
     Key([mod], "g", lazy.spawn('rofi -show run')),
     Key([mod], "r", lazy.spawn('rofi -modi combi -show combi -combi-modi drun,run')),
 
