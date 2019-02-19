2f47ab1a1d0421330ec99c7e9a5007c23820e911
reisub0 <reisub0@gmail.com>
2019-02-19 22:42:50 +0530

    Adds ic abbr -> fish
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -52,6 +52,7 @@ if not set -q abbrs_initialized
     abbr g 'googler --notweak --noua'
     abbr h 'history | grep '
     abbr hlp 'less /etc/bash.bashrc_help'
+    abbr ic 'iC c'
     abbr ip 'sudo ip'
     abbr ipa 'sudo ip a'
     abbr mkbz2 'tar -cvjf'
