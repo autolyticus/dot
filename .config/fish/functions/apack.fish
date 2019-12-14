f695662faf618ca41f552a4f700543e2cceaa595
reisub0 <reisub0@gmail.com>
2019-12-15 00:32:33 +0530

    Apack now completes and supports AUR
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/functions/apack.fish
+++ .config/fish/functions/apack.fish
@@ -1,6 +1,6 @@
-function apack
-	pacman -S --needed "$argv"; and echo "$argv" >>~/.local/.packlist
+function apack --wraps 'yay -S'
+    yay -S --needed "$argv"
+    and echo "$argv" >>~/.local/.packlist
     sort ~/.local/.packlist | uniq >~/.local/.packlist.new
     mv -f ~/.local/.packlist.new ~/.local/.packlist
 end
-
