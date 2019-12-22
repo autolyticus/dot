519ea7ca3fcb356fb6a0348d419b4de6da3c4cf5
reisub0 <reisub0@gmail.com>
2019-12-22 18:06:20 +0530

    Renames variables to 00-vars, adds guard
    Signed-off-by: reisub0 <reisub0@gmail.com>

---
+++ .config/fish/conf.d/00-vars.fish
@@ -0,0 +1,8 @@
+if not set -q vars_initialized
+    set -U vars_initialized
+
+    set -U FZF_COMPLETE 0
+    set -U FZF_FIND_FILE_COMMAND "fd --hidden --no-ignore-vcs --exclude .git \$dir"
+    set -U FZF_CD_COMMAND "fd --hidden --no-ignore-vcs --exclude .git --type d"
+    set -U FZF_CD_WITH_HIDDEN_COMMAND $FZF_CD_COMMAND
+end
