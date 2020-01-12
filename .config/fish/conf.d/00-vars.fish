68088a814b0db46ef8ba297e250da54b15b9bb9c
Govind KP <reisub0@gmail.com>
2020-01-12 22:12:10 +0530

    Re-enables fzf completion for Fish, fixes Fzf find command
    Signed-off-by: Govind KP <reisub0@gmail.com>

--- .config/fish/conf.d/00-vars.fish
+++ .config/fish/conf.d/00-vars.fish
@@ -4,9 +4,10 @@ if not set -q vars_initialized
         set -e $i
     end
 
-    # TODO Re-enable completion when stabilised
-    # set -U FZF_COMPLETE 0
-    set -U FZF_FIND_FILE_COMMAND "fd --hidden --no-ignore-vcs --exclude .git \$dir"
+    # Completion still has some bugs
+    set -U FZF_COMPLETE 0
+
+    set -U FZF_FIND_FILE_COMMAND "fd --hidden --no-ignore-vcs --exclude .git . \$dir"
     set -U FZF_CD_COMMAND "fd --hidden --no-ignore-vcs --exclude .git --type d"
     set -U FZF_CD_WITH_HIDDEN_COMMAND $FZF_CD_COMMAND
 end
