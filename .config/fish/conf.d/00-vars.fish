f6a1fdfef1e2c559d661b35de6715d2d5f947b6e
Govind KP <reisub0@gmail.com>
2020-01-17 13:54:32 +0530

    Reorders guard clause
    Signed-off-by: Govind KP <reisub0@gmail.com>

--- .config/fish/conf.d/00-vars.fish
+++ .config/fish/conf.d/00-vars.fish
@@ -1,5 +1,4 @@
 if not set -q vars_initialized
-    set -U vars_initialized
     for i in (cat (status filename) | sed '/set -U/s/^\s*#//' | egrep '^\s*set -U' | awk '{print $3}')
         set -e $i
     end
@@ -7,7 +6,10 @@ if not set -q vars_initialized
     # Completion still has some bugs
     # set -U FZF_COMPLETE 0
 
+    echo "SETTING VARS AGAIN"
     set -U FZF_FIND_FILE_COMMAND "fd --hidden --no-ignore-vcs --exclude .git . \$dir"
     set -U FZF_CD_COMMAND "fd --hidden --no-ignore-vcs --exclude .git --type d"
     set -U FZF_CD_WITH_HIDDEN_COMMAND $FZF_CD_COMMAND
+
+    set -U vars_initialized
 end
