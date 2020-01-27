d57e415df20d2285f2f53d64b5ad246042e5343a
reisub0 <reisub0@gmail.com>
2020-01-27 08:56:57 +0530

    Changes youtube-dl command to refer to music library var
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -134,7 +134,7 @@ if not set -q abbrs_initialized
     abbr wifi-menu 'sudo wifi-menu'
     abbr wireshark 'sudo wireshark'
     abbr yay 'yay --aur'
-    abbr yd 'youtube-dl -o '"'"'/media/d/Music/%(title)s.%(ext)s'"'"' -x --audio-format mp3 --audio-quality 0'
+    abbr yd 'youtube-dl -o "$MUSIC_LIBRARY/%(title)s.%(ext)s" -x --audio-format mp3 --audio-quality 0'
     abbr youtube-dl 'youtube-dl --external-downloader aria2c'
     abbr ys 'yay -S --noconfirm'
     abbr yu 'yay -Syu --aur --noconfirm'
