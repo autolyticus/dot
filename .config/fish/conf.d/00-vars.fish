d6b56dede9f48a5e5d1c6638b961760156c2772e
reisub0 <reisub0@gmail.com>
2020-02-18 10:34:01 +0530

    Sets colour scheme for bobthefish to base16
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/00-vars.fish
+++ .config/fish/conf.d/00-vars.fish
@@ -7,6 +7,7 @@ if not set -q vars_initialized
     # set -U FZF_COMPLETE 0
 
     echo "SETTING VARS AGAIN"
+    set -U theme_color_scheme base16
     set -U FZF_FIND_FILE_COMMAND "fd --hidden --no-ignore-vcs --exclude .git . \$dir"
     set -U FZF_CD_COMMAND "fd --hidden --no-ignore-vcs --exclude .git --type d"
     set -U FZF_CD_WITH_HIDDEN_COMMAND $FZF_CD_COMMAND
