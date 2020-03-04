b016e2ee0ba83017022dbd50329c2ac1db585e05
reisub0 <reisub0@gmail.com>
2020-03-04 14:25:20 +0000

    Adds essential packages for auto bootstrap
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/01-fisher-bootstrap.fish
+++ .config/fish/conf.d/01-fisher-bootstrap.fish
@@ -2,6 +2,7 @@
 if not functions -q fisher
     set -q XDG_CONFIG_HOME
     or set XDG_CONFIG_HOME ~/.config
+    sudo pacman -Syu --noconfirm fd bat lsd fasd
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     curl https://raw.githubusercontent.com/reisub0/dot/master/.config/fish/fishfile -o ~/.config/fish/fishfile
     fish -c fisher
