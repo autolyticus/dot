b595b3c5e5a2014e8960885e0daac43439366148
reisub0 <reisub0@gmail.com>
2020-07-18 13:23:21 +0530

    Adds i3 autostart
    Signed-off-by: reisub0 <reisub0@gmail.com>

---
+++ .config/i3/autostart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+exec 1>/tmp/autostart.log 2>&1
+
+runCommand() {
+    pgrep "$(echo "$@" | cut -d ' ' -f 1)" ||
+    ("$@" > /tmp/"$1.log" 2>&1) &
+}
+
+runCommand mpd
+runCommand nm-applet
+runCommand ckb-next
+runCommand rescuetime
+$HOME/.config/polybar/launch.sh
+# runCommand waybar -l trace
+# runCommand sway-alttab
+# runCommand mako
+# runCommand idler
+# runCommand delLock
+# runCommand xidlehook --not-when-audio --timer primary 240 'locker' 'killall i3lock' --timer normal 5 'xset dpms force off' ''
