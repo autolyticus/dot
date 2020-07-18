f0f0bfaa9611fdc0239326fbb536083907005665
reisub0 <reisub0@gmail.com>
2020-07-18 13:28:15 +0530

    Forgot to add this commit for the previous
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -6,15 +6,6 @@ runCommand() {
     pkill "$(echo "$@" | cut -d ' ' -f 1)"
     ("$@" > /tmp/"$1.log" 2>&1) &
 }
-xset r rate 250 30 &
-trackBallID=$( (
-    xinput list 2>/dev/null
-    true
-) | grep M570 | cut -f 2 | cut -d'=' -f 2)
-xinput set-prop $trackBallID "Coordinate Transformation Matrix" .9 0 0 0 .9 0 0 0 2.5 &
-xinput set-prop $trackBallID "libinput Accel Speed" .8 &
-xinput set-prop $trackBallID "libinput Middle Emulation Enabled" 1 &
-
 
 runCommand conky
 runCommand feh --bg-scale ~/.config/awesome/themes/multicolor/vio.jpg
