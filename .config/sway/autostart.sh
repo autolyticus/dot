abbf68b516338b7602722b900679cea85bb085e8
reisub0 <reisub0@gmail.com>
2020-07-18 13:29:49 +0530

    Sway autostart launchers (deprecated)
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -3,16 +3,18 @@
 exec 1>/tmp/autostart.log 2>&1
 
 runCommand() {
-    pkill "$(echo "$@" | cut -d ' ' -f 1)"
+    pgrep "$(echo "$@" | cut -d ' ' -f 1)" ||
     ("$@" > /tmp/"$1.log" 2>&1) &
 }
 
 runCommand mpd
-# runCommand nm-applet --indicator
-runCommand waybar
-runCommand sway-alttab
-runCommand mako
-runCommand idler
-# runCommand rescuetime
+runCommand nm-applet
+runCommand ckb-next
+$HOME/.config/polybar/launch.sh
+# runCommand waybar -l trace
+# runCommand sway-alttab
+# runCommand mako
+# runCommand idler
+runCommand rescuetime
 # runCommand delLock
 # runCommand xidlehook --not-when-audio --timer primary 240 'locker' 'killall i3lock' --timer normal 5 'xset dpms force off' ''
