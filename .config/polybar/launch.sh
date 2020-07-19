3e86e88fd2167f74ee2a30d051f57c39b15332e8
reisub0 <reisub0@gmail.com>
2020-07-19 14:39:02 +0530

    Makes polybar launch on multiple monitors
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -r main &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+    done
+else
+    polybar --reload main &
+fi
 
 echo "Polybar launched..."
