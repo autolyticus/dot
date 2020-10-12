8efc34591055a6cecfda8266959a43563117274a
reisub0 <reisub0@gmail.com>
2020-10-13 01:57:53 +0530

    Handle case when monitor is not connected gracefully
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,16 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
-    PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-    OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+    PRIMARY=$(xrandr --query | grep "connected" | grep -v "disconnected" | grep "primary" | cut -d" " -f1)
+    OTHERS=$(xrandr --query | grep "connected" | grep -v "disconnected" | grep -v "primary" | cut -d" " -f1)
 
     # Launch on primary monitor with tray enabled
     MONITOR=$PRIMARY TRAY_POS=left polybar --reload main &
 
     # Launch Polybar, on other monitors without tray
-    for m in "$OTHERS"; do
-        MONITOR=$m polybar --reload main &
-    done
+    if [ -n "$OTHERS" ]; then
+        for m in "$OTHERS"; do
+            MONITOR=$m polybar --reload main &
+        done
+    fi
 else
     polybar --reload main &
 fi
