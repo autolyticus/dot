e808052afe05f3b97411bfa56cf6195d84b1e263
reisub0 <reisub0@gmail.com>
2020-11-11 18:20:59 +0530

    Adds robust autostart to i3
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -3,20 +3,28 @@
 exec 1>/tmp/autostart.log 2>&1
 
 runCommand() {
-    pgrep "$(echo "$@" | cut -d ' ' -f 1)" ||
+    pgrep -x "$(echo "$@" | cut -d ' ' -f 1)" ||
     ("$@" > /tmp/"$1.log" 2>&1) &
 }
 
 runCommand mpd
+runCommand redshift -l 20.5937:78.9629 -t 6500:3000
 runCommand conky
 runCommand nm-applet
 runCommand rescuetime
 runCommand numlockx
 runCommand i3-alttab
+runCommand ckb-next -b
 runCommand feh --bg-scale ~/.config/awesome/themes/powerarrow-dark/wall.jpg
+runCommand remmina -i
+runCommand nextcloud --background
+sudo $HOME/.local/bin/fixAudio &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+# runCommand gnome-session
 $HOME/.config/polybar/launch.sh &
-sudo systemctl restart ckb-next-daemon &
-runCommand ckb-next
+systemctl --user start xfce4-notifyd &
+sleep 3 && sudo systemctl restart ckb-next-daemon &
+xset r rate 250 35 &
 # runCommand waybar -l trace
 # runCommand sway-alttab
 # runCommand mako
