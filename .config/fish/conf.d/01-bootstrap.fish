63f7fc7aabe9e35fd98eb2541fb410b0abec55c1
reisub0 <reisub0@gmail.com>
2021-01-23 13:24:44 +0530

    Adds pyenv sourcing to fish
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- .config/fish/conf.d/01-bootstrap.fish
+++ .config/fish/conf.d/01-bootstrap.fish
@@ -8,4 +8,9 @@ if not functions -q fisher
     fish -c fisher
 end
 replay source $HOME/.bash_profile
+
+# Set up Pyenv
+if command -v pyenv 1>/dev/null 2>&1
+    pyenv init - | source
+end
 # cd $HOME
