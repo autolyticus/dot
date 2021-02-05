f839655d0135fdd9288a0c6b7f1c9f0a047dc0b5
Govind <govindk@mathworks.com>
2021-02-05 19:53:20 +0530

    Removes apack as it conflicts with atool function
    Signed-off-by: Govind <govindk@mathworks.com>

--- .config/fish/functions/apack.fish
+++
@@ -1,6 +0,0 @@
-function apack --wraps 'yay -S'
-    yay -S --needed "$argv"
-    and echo "$argv" >>~/.local/.packlist
-    sort ~/.local/.packlist | uniq >~/.local/.packlist.new
-    mv -f ~/.local/.packlist.new ~/.local/.packlist
-end
